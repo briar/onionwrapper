b3c0faaf4887ac26852de569978675b8ee32693c
Torsten Grote <t@grobox.de>
2023-05-02 15:01:44 -0300

    Add factory for easier creating of dependencies

---
+++ onionwrapper-core/src/main/java/org/briarproject/onionwrapper/CircumventionProviderFactory.java
@@ -0,0 +1,12 @@
+package org.briarproject.onionwrapper;
+
+import org.briarproject.nullsafety.NotNullByDefault;
+
+@NotNullByDefault
+public class CircumventionProviderFactory {
+
+    public static CircumventionProvider createCircumventionProvider() {
+        return new CircumventionProviderImpl();
+    }
+
+}
