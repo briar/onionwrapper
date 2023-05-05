025f9911d8ba69b432aebec15194a16b8eafc28f
Torsten Grote <t@grobox.de>
2023-05-05 11:16:03 -0300

    Add JavaLocationUtils and factory

---
+++ onionwrapper-java/src/main/java/org/briarproject/onionwrapper/JavaLocationUtilsFactory.java
@@ -0,0 +1,9 @@
+package org.briarproject.onionwrapper;
+
+public class JavaLocationUtilsFactory {
+
+    public static JavaLocationUtils createJavaLocationUtils() {
+        return new JavaLocationUtils();
+    }
+
+}
